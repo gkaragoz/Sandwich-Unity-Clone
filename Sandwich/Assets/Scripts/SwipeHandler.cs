41ed46e208f15bb7ff0c3e75210eea122a811e52
gkaragoz <gkaragoz96@gmail.com>
2020-08-14 12:36:45 +0300

    Create SwipeHandler.cs

---
+++ Sandwich/Assets/Scripts/SwipeHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwipeHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public static Action<Direction> onSwiped;
+
+    public enum Direction {
+        None,
+        Up,
+        Right,
+        Down,
+        Left
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Direction direction = GetDirection(eventData.delta);
+        Debug.Log("SWIPED TO: " + direction);
+
+        onSwiped?.Invoke(GetDirection(eventData.delta));
+    }
+    public void OnDrag(PointerEventData eventData) {
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+    }
+
+    private Direction GetDirection(Vector2 delta) {
+        if (delta.x == 0 && delta.y == 0) {
+            return Direction.None;
+        }
+
+        // I. Area
+        if (delta.x >= 0 && delta.y >= 0) {
+            if (Math.Abs(delta.x) > Math.Abs(delta.y)) {
+                return Direction.Right;
+            } else {
+                return Direction.Up;
+            }
+        } 
+        // II. Area
+        else if (delta.x <= 0 && delta.y >= 0) {
+            if (Math.Abs(delta.x) > Math.Abs(delta.y)) {
+                return Direction.Left;
+            } else {
+                return Direction.Up;
+            }
+        }
+        // III. Area
+        else if (delta.x <= 0 && delta.y <= 0) {
+            if (Math.Abs(delta.x) > Math.Abs(delta.y)) {
+                return Direction.Left;
+            } else {
+                return Direction.Down;
+            }
+        }
+        // IV. Area
+        else if (delta.x >= 0 && delta.y <= 0) {
+            if (Math.Abs(delta.x) > Math.Abs(delta.y)) {
+                return Direction.Right;
+            } else {
+                return Direction.Down;
+            }
+        }
+
+        return Direction.None;
+    }
+
+}
